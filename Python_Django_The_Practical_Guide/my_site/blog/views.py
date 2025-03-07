1db26dda3166b6656be2fed98948534bda1f802f
rfpacheco <ronnyfph@gmail.com>
2025-03-07 18:54:49 +0100

    Added URL & Views

--- Python_Django_The_Practical_Guide/my_site/blog/views.py
+++ Python_Django_The_Practical_Guide/my_site/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def starting_page(request):
+    return None
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
