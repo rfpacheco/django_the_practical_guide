c23f870f1c1db7401f102f9ebad44be42ab016f0
rfpacheco <ronnyfph@gmail.com>
2025-03-07 21:31:10 +0100

    Added posts pages and styles

--- Python_Django_The_Practical_Guide/my_site/blog/views.py
+++ Python_Django_The_Practical_Guide/my_site/blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def starting_page(request):
-    return None
+    return render(request, "blog/index.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 def post_detail(request):
     pass
