10adce96f18c2da45205061b49167668c47b5d44
ronnyfph <ronnyfph@gmail.com>
2025-03-19 00:13:29 +0100

    Add slug field to Book model with auto-generation logic
    Added a `slug` field to the `Book` model for better URL structuring. Implemented automatic slug generation based on the book's title in the `save` method. Also created a migration to apply the changes to the database.

--- Python_Django_The_Practical_Guide/book_store/book_outlet/models.py
+++ Python_Django_The_Practical_Guide/book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Book(models.Model):
@@ -8,9 +9,14 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)  # e.g. Harry Potter 1 ==> harry-potter-1
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
