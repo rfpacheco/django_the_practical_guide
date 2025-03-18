4300b8779d9634757658666704d00b346862addc
ronnyfph <ronnyfph@gmail.com>
2025-03-18 23:28:39 +0100

    Add book detail view and template rendering
    Implemented a new `book_detail` view to retrieve and display details of individual books. Updated `urls.py` to include a path for book details and created a corresponding `book_detail.html` template with enhanced structure and dynamic content.

--- Python_Django_The_Practical_Guide/book_store/book_outlet/views.py
+++ Python_Django_The_Practical_Guide/book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
@@ -8,4 +9,18 @@ def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {
         "books": books
+    })
+
+# noinspection PyShadowingBuiltins
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404("Book not found")
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
     })
