f6d28c4223fb87e894c51a4f268475d754aa782b
ronnyfph <ronnyfph@gmail.com>
2025-03-18 23:54:51 +0100

    Add named URL and absolute URL for book detail view
    Added a `name` attribute to the book detail URL pattern for reverse URL resolution. Enhanced the `Book` model with a `get_absolute_url` method for generating detail page links. Updated the index template to include clickable book titles linking to their detail pages.

--- Python_Django_The_Practical_Guide/book_store/book_outlet/models.py
+++ Python_Django_The_Practical_Guide/book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):
@@ -7,6 +8,9 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
