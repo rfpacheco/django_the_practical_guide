26cb26ebee1aaada1e6d5be93eb5ff2930cb85b7
ronnyfph <ronnyfph@gmail.com>
2025-03-21 14:22:26 +0100

    Add Country model and enhance relationships in models
    Introduced a new Country model with fields for name and code. Updated Address and Country models to include string representations and verbose name plurals. Modified Book to associate with Country using a ManyTo-Many field and fixed a typo in Author's address field name. Updated admin to register Address and Country models.

--- Python_Django_The_Practical_Guide/book_store/book_outlet/models.py
+++ Python_Django_The_Practical_Guide/book_store/book_outlet/models.py
@@ -4,16 +4,32 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.street}, {self.postal_code} {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    adress = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -27,6 +43,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True ,null=False, db_index=True)  # e.g. Harry Potter 1 ==> harry-potter-1
+    publish_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
