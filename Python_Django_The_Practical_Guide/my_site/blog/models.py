5737abdc94f2a460357d47bfc858e72c9a2a80e6
ronnyfph <ronnyfph@gmail.com>
2025-04-22 10:51:39 +0200

    Added data via Admin

--- Python_Django_The_Practical_Guide/my_site/blog/models.py
+++ Python_Django_The_Practical_Guide/my_site/blog/models.py
@@ -5,10 +5,19 @@ from django.db import models
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -18,3 +27,7 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+
+
