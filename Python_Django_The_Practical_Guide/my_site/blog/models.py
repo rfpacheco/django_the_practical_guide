8d5a025ed961955627fd322403d4042e0e716058
rfpacheco <ronnyfph@gmail.com>
2025-05-08 10:37:25 +0200

    Add Author model to blog app
    Introduced a new Author model with fields for first name, last name, and email address. This change sets up the foundation for associating blog posts with authors in the future.

--- Python_Django_The_Practical_Guide/my_site/blog/models.py
+++ Python_Django_The_Practical_Guide/my_site/blog/models.py
@@ -1,7 +1,14 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
 
+
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.CharField()
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
