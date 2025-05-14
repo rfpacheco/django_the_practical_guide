f2fbdc8188b6ad2cc578104aecb294a2cc0a57fd
rfpacheco <ronnyfph@gmail.com>
2025-05-14 14:48:30 +0200

    Add form validation and thank-you page handling
    Introduce basic form validation to check for empty or overly long usernames and display an error message. Implement a thank-you page, redirecting users upon successful form submission, and register its URL route.

--- Python_Django_The_Practical_Guide/reviews_project/reviews/urls.py
+++ Python_Django_The_Practical_Guide/reviews_project/reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.review, name="index"),
+    path("thank-you", views.thank_you)
 ]
--- Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
+++ Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
+    if request.method == "POST":
+        enter_username = request.POST["username"]
+
+        if enter_username == "" or len(enter_username) >= 100:
+            return render(request, "reviews/review.html",
+                          {"has_error" : True})
+
+        print(enter_username)
+        return HttpResponseRedirect("/thank-you")
+
     return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
