09f1a5253df2d8ed3f71b11166eafbc61f105b96
rfpacheco <ronnyfph@gmail.com>
2025-05-18 17:46:49 +0200

    Add validation and labels to ReviewForm fields
    Improved the `ReviewForm` with user-friendly labels and custom error messages for better UX. Updated the view to ensure cleaner instantiation and handling of the form during GET and POST requests. This enhances form usability and overall code clarity.

--- Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
+++ Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .forms import ReviewForm
+
+
 # Create your views here.
 
 def review(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
 
-    form = ReviewForm()
+    else:
+        form = ReviewForm()
 
     return render(request, "reviews/review.html", {
         "form": form
