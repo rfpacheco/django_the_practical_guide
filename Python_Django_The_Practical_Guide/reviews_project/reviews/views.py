dbd180f41b2610be3dcc370982bbe504672b6296
rfpacheco <ronnyfph@gmail.com>
2025-06-01 23:25:35 +0200

    Refactor SingleReviewView to use DetailView for improved model-based handling and simplify URL patterns with <int:pk>.

--- Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
+++ Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
@@ -2,15 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .forms import ReviewForm
 from .models import Review
 
 
 # Create your views here.
-
-
 class ReviewView(View):
     def get(self, request):
         form = ReviewForm()
@@ -43,17 +41,11 @@ class ReviewsListView(ListView):
     model = Review
     context_object_name = "reviews"
 
-    def get_queryset(self):
-        base_query = super().get_queryset()
-        data = base_query.filter(rating__gt=4)
-        return data
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
 
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = self.kwargs["id"]
-        selected_review = Review.objects.get(pk=review_id)
-        context["review"] = selected_review
-        return context
+    model = Review
