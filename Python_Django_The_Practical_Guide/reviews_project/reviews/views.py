41d7a1320bd19e0df99525e220f023ca64905af1
ronnyfph <ronnyfph@gmail.com>
2025-06-27 20:12:47 +0200

    Add favorite review functionality with session-based storage
    Introduced `AddFavoriteView` to handle favoriting reviews via POST requests. Extended `SingleReviewView` to display if a review is marked as favorite and updated `single_review.html` with a form for marking favorites. Adjusted URLs to include the new view.

--- Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
+++ Python_Django_The_Practical_Guide/reviews_project/reviews/views.py
@@ -16,6 +16,7 @@ class ReviewView(CreateView):
     template_name = "reviews/review.html"
     success_url = "/thank-you"
 
+
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
 
@@ -23,6 +24,7 @@ class ThankYouView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["message"] = "This works!"
         return context
+
 
 class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
@@ -34,6 +36,21 @@ class ReviewsListView(ListView):
     #     data = base_query.filter(rating__gt=4)
     #     return data
 
+
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get('favorite_review')
+        context['is_favorite'] = favorite_id == str(loaded_review.id)
+        return context
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['favorite_review'] = review_id
+        return HttpResponseRedirect('/reviews/' + review_id)
